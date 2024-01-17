dd87ef933702d4a90578c0400537e80b4464a6ec
Jen Triñanes <jen.trinanes@whitecobalt.com>
2024-01-17 18:52:16 +0800

    Added Integration Test for API

---
+++ UKRainfall.API.IntegrationTests/Controllers/RainfallControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using UKRainfall.API.IntegrationTests.Base;
+using UKRainfall.Application.Features.Readings;
+
+namespace UKRainfall.API.IntegrationTests.Controllers
+{
+    public class RainfallControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public RainfallControllerTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/rainfall/id/289102TP/readings");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<ReadingListVm>>(responseString);
+
+            Assert.IsType<List<ReadingListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
---
+++ UKRainfall.Api/Controllers/RainfallController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UKRainfall.Application.Features.Readings;
+
+namespace UKRainfall.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RainfallController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RainfallController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("id/{stationId}/readings", Name = "GetRainfall")]
+        public async Task<ActionResult<List<ReadingListVm>>> GetEventById(string stationId)
+        {
+            var getRainfallReadingQuery = new GetReadingsListQuery() { StationId = stationId };
+            return Ok(await _mediator.Send(getRainfallReadingQuery));
+        }
+    }
+}
